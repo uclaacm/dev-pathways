a093eda514b7384ae3a351a16f76ca9056be5526
EinarBalan <einar.balan@gmail.com>
2021-01-25 22:53:12 -0800

    Added Team cards and data structure

--- src/components/TeamSection/TeamSection.js
+++ src/components/TeamSection/TeamSection.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './TeamSection.css';
+import '../TeamCard/TeamCard';
+import teams from "../../data/teams";
+import TeamCard from '../TeamCard/TeamCard';
 
 const TeamSection = () => {
     return (
         <div className="team-container">
             <h2>Team Overviews</h2>
+            <div className="cards-grid">
+                {teams.map(team =>
+                    <TeamCard team={team} />
+                )}
+            </div>
         </div>
     );
 }
---
+++ src/data/teams.js
@@ -0,0 +1,49 @@
+import teachla from "../img/teachla.svg";
+import hack from "../img/hack.svg";
+import board from "../img/board.svg";
+import w from "../img/w.svg";
+
+const teams = [
+    {
+        logotype: teachla,
+        technologies: ["HTML/CSS", "Jekyll/Liquid", "React"],
+        projects: ["Static Site", "Learning Labs"],
+    },
+    {
+        logotype: hack,
+        technologies: ["HTML/CSS", "Jekyll/Liquid", "React"],
+        projects: ["Static Site", "Learning Labs"],
+    },
+    {
+        logotype: board,
+        technologies: ["HTML/CSS", "Jekyll/Liquid", "React"],
+        projects: ["Static Site", "Learning Labs"],
+    },
+    {
+        logotype: w,
+        technologies: ["HTML/CSS", "Jekyll/Liquid", "React"],
+        projects: ["Static Site", "Learning Labs"],
+    },
+    {
+        logotype: board,
+        technologies: ["HTML/CSS", "Jekyll/Liquid", "React"],
+        projects: ["Static Site", "Learning Labs"],
+    },
+    {
+        logotype: board,
+        technologies: ["HTML/CSS", "Jekyll/Liquid", "React"],
+        projects: ["Static Site", "Learning Labs"],
+    },
+    {
+        logotype: board,
+        technologies: ["HTML/CSS", "Jekyll/Liquid", "React"],
+        projects: ["Static Site", "Learning Labs"],
+    },
+    {
+        logotype: board,
+        technologies: ["HTML/CSS", "Jekyll/Liquid", "React"],
+        projects: ["Static Site", "Learning Labs"],
+    },
+];
+
+export default teams;
