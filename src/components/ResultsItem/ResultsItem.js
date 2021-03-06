f789dcfe3fb9e13e530c7ad8c26d12b9773b2316
nicole-chang <nchang20@ucla.edu>
2021-03-05 19:29:12 -0800

    added icons and descriptions

--- src/components/ResultsItem/ResultsItem.js
+++ src/components/ResultsItem/ResultsItem.js
@@ -2,11 +2,23 @@ import React from 'react'
 import './ResultsItem.css'
 
 const Home = props => {
+    
+
     return (
         <div className="result" key={props.item.id}>
-        <a href={props.link} className="titleLink">{props.item.resource.name}</a>
-        <p>{props.item.resource.description ?? "No description available."}{props.otherSources}</p>
-        <footer>{props.item.resource.source}</footer>
+            {props.item.resource.icon ?
+            <img className="result-icon" src={props.item.resource.icon}></img> : ""}
+            
+            <div className="result-body">
+            <a href={props.link} className="titleLink">{props.item.resource.name}</a>
+            
+            <div className="description">{props.item.resource.description ?? "No description available."}</div>
+            
+            <footer>
+                {props.item.resource.source}
+                {props.otherSources}
+            </footer>
+            </div>
         </div> 
     )
 }
