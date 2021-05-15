bfbcc9bdf7e8fb32d304e0d4b29dc5e94a0bf10c
Advaith G <67539191+advaithg@users.noreply.github.com>
2021-05-16 01:59:04 +0530

    typed.js added

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,8 +3,23 @@ import './SearchBar.css';
 import Clickable from '../Clickable/Clickable';
 import line from './line.svg';
 import search from './search.svg';
+import Typed from 'typed.js';
 
 //TODO: implement search bar query cycling animation
+
+const typed4 = () => {
+    return (
+        new Typed('#typed4', {
+            strings: ['Some strings without', 'Some HTML', 'Chars'],
+            typeSpeed: 0,
+            backSpeed: 0,
+            attr: 'placeholder',
+            bindInputFocusEvents: true,
+            loop: true
+          })
+    );
+}
+
 
 const SearchBar = props => {
     return (
@@ -21,7 +36,7 @@ const SearchBar = props => {
                 onChange={props.onChange}
                 onKeyDown={props.onKeyDown}
                 autoComplete="off"
-                placeholder="What do you want to learn?"
+                placeholder= {<typed4/>}
             />
         </div>
     );
