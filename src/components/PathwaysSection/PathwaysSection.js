fa9cf132213345b9e3bc67dc7203865cb6dd7fe6
EinarBalan <einar.balan@gmail.com>
2021-06-29 11:09:33 -0700

    Adjusted wording on pathways section

--- src/components/PathwaysSection/PathwaysSection.js
+++ src/components/PathwaysSection/PathwaysSection.js
@@ -10,11 +10,11 @@ const PathwaysSection = () => {
             <div className="pathway-container">
                 <img src={pathway1} alt=""/>
                 <div className="pathway-info">
-                    Want to learn a technology but never know where to start? Dev Pathways will guide you through it! Simply answer a few questions and we'll take care of the rest. 
+                    Want to learn a technology but don't know where to start? Dev Pathways will guide you through it! Simply answer a few questions and we'll take care of the rest. 
 
                     <br/><br/>
                     
-                    Based on your answers, a unique learning pathway will be generated for you, full of resources created by ACM members as well as externally created resources! If you're a new developer at ACM, welcome to the team.
+                    Based on your answers, a unique learning pathway full of both ACM and external resources will be generated! Soon enough, you'll be ready to contribute to all of our projects at ACM. Welcome to the team.
                     <SolidButton className="gen-pathway-button" text="Generate"/>
                 </div>
             </div>
