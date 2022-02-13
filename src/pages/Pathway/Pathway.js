d07fa258deaa696d3a94e92c0be546ae5780bc38
Michael Yu <michaelyu678@gmail.com>
2022-02-12 23:22:00 -0800

    initial react-pdf setup

--- src/pages/Pathway/Pathway.js
+++ src/pages/Pathway/Pathway.js
@@ -1,4 +1,5 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
+import { BlobProvider, PDFDownloadLink, Document, Page, usePDF, Text } from '@react-pdf/renderer';
 import PageHeader from '../../components/PageHeader/PageHeader';
 import PathwayHeader from '../../img/pathway-header.svg';
 import PathDay from '../../components/PathDay/PathDay';
@@ -7,11 +8,21 @@ import resources from '../../data/resources';
 import './Pathway.css';
 
 const Pathway = () => {
-    const {quizResults} = useContext(QuizContext);
+    const { quizResults } = useContext(QuizContext);
     // start from here...
     console.log(quizResults);
-    
+
     const example_resources = resources[3].links;
+
+    const MyDoc = () => (
+        <Document>
+            <Page>
+                <Text>My document data</Text>
+            </Page>
+        </Document>
+    );
+
+    //const [instance, updateInstance] = usePDF({ document: MyDoc });
 
     return (
         <div className="pathway-page">
@@ -20,17 +31,28 @@ const Pathway = () => {
                 subheader="react  •  beginner  •  videos  •  1 week"
                 img={PathwayHeader}
                 imgStyle={{
-                    backgroundSize: "423px 297px",
-                    backgroundPosition: "100%",
+                    backgroundSize: '423px 297px',
+                    backgroundPosition: '100%',
                 }}
             />
-            {example_resources.map((resource, index) => 
+            {example_resources.map((resource, index) => (
                 <PathDay index={index + 1} resource={resource} />
-            )}
+            ))}
             <h2>Come back later?</h2>
-            <p3>You can come back later and your pathway will be saved! Alternatively,
-                 you can <a href="/quiz" rel="noreferrer noopener">start over</a>.</p3>
-
+            <p3>
+                You can come back later and your pathway will be saved! Alternatively, you can{' '}
+                <a href="/quiz" rel="noreferrer noopener">
+                    {' '}
+                    start over
+                </a>
+            </p3>
+            <BlobProvider document={<MyDoc />}>
+                {({ url }) => (
+                    <a href={url} target="_blank">
+                        Open in new tab
+                    </a>
+                )}
+            </BlobProvider>
         </div>
     );
 };
