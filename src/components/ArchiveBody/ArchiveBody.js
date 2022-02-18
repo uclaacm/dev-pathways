2a514d415a17e2b348d642110dce25661e54a540
PierceCCH <pierce.chongch@gmail.com>
2022-02-17 18:23:36 -0800

    Sorted buttons, removed ArchiveList component, made changes to CSS

--- src/components/ArchiveBody/ArchiveBody.js
+++ src/components/ArchiveBody/ArchiveBody.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import teams from '../../data/teams'
-import ArchiveList from '../ArchiveList/ArchiveList';
 import TeamButton from '../TeamButton/TeamButton'
+import ArchiveCommittee from '../CommitteeArchive/ArchiveCommittee'
 import "./ArchiveBody.css";
 
 function ArchiveBody() {
+
+  const [committee, setCommittee] = useState("studio")
+
+  const refArray = ["studio", "icpc", "design", "cyber", "teachla", "w", "ai", "hack"]
+  const sortedTeams = JSON.parse(JSON.stringify(teams))
+
   return (
     <div className='ArchiveBody'>
         <div className='button-col'>
-            {teams.map((team) => (team.name !== "board") && 
+            {sortedTeams.sort((a,b) => refArray.indexOf(a.name) - refArray.indexOf(b.name)).map((team) => (team.name !== "board") && 
             <TeamButton
                 className = 'team-button'
                 icon = {team.logotype}
                 alt = {team.name}
-                onClick = {() => (console.log("click"))}
+                onClick = {() => (setCommittee(team.name))}
             />)}
         </div>
         <div className='list-content'>
             <div className='static-bar'></div>
-            <h4 className='list-content-header'>Committee</h4>
-            <ArchiveList />
         </div>
+        <ArchiveCommittee committeeName={committee}/>
     </div>
   );
 }
