00616d6339c531bccc80d84482acec043c41f41e
Pierce Chong <48197758+PierceCCH@users.noreply.github.com>
2022-02-22 14:27:21 -0800

    built general layout of archive page (#125)
    * built general layout of archive page
    
    * Sorted buttons, removed ArchiveList component, made changes to CSS
    
    * renamed source names to match those in resources.js

--- src/pages/Archive/Archive.js
+++ src/pages/Archive/Archive.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import ArchiveBody from '../../components/ArchiveBody/ArchiveBody.js';
+import PageHeader from '../../components/PageHeader/PageHeader';
+import ArchiveHeader from "../../img/archive-header.svg";
 
 function Archive() {
-  return <div> <h1>ACM Archive</h1></div>;
+  return (
+    <div className= "archive"> 
+        <PageHeader 
+          header="ACM Archive"
+          subheader="A list of the previous workshops given by ACM!"
+          img={ArchiveHeader}
+          imgStyle={{
+            backgroundSize: "423px 297px",
+            backgroundPosition: "100%",
+          }}
+        />
+        <ArchiveBody />
+    </div>
+    );
 }
 
 export default Archive;
