54fe3f185f68c6dea0ce0631a7a3396738f6ce8c
samgivian <samgivian2015@gmail.com>
2022-03-21 08:40:45 -0700

    Update ResultFilter.js

--- src/components/ResultFilter/ResultFilter.js
+++ src/components/ResultFilter/ResultFilter.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React from "react";
 import "./ResultFilter.css";
 import SolidButton from "../SolidButton/SolidButton";
 import checkboxes from "../../data/checkboxes";
